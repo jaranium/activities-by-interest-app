a9031a86eedb50ba2162ec6868757cef0ef30fb5
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-03-30 10:36:17 -0700

    Added files to client folder

---
+++ client/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        Hi there
+      </div>
+    )
+  }
+}
+
+export default App;
