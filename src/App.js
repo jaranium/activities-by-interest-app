641f41889c463c5ea9f00d1accc0226a6be20075
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-03-30 12:53:08 -0700

    created profile container, hot reloading working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ProfileContainer from './containers/ProfileContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Welcome to EventBite!</h1>
+          <p className="App-intro">Find fun events based on  your interests!</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <ProfileContainer  />
       </div>
     );
   }
---
+++ src/containers/ProfileContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import from child components...
+
+class ProfileContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>I'm the profileContainer</h2>
+      </div>
+    )
+  }
+}
+
+export default ProfileContainer;
