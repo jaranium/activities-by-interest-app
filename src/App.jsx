eb61d119eb606ae50da92a98d093a3855ec00e00
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-03-30 19:15:19 -0700

    front-end updates, profile container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import ProfileContainer from './containers/ProfileContainer';
+import MainContainer from './containers/MainContainer.jsx';
 
 class App extends Component {
   render() {
@@ -10,9 +10,9 @@ class App extends Component {
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to EventBite!</h1>
-          <p className="App-intro">Find fun events based on  your interests!</p>
+          <p className="App-intro">Find fun events based on  your interests! :) :) :) </p>
         </header>
-      <ProfileContainer  />
+      <MainContainer  />
       </div>
     );
   }
--- src/containers/ProfileContainer.jsx
+++ src/containers/ProfileContainer.jsx
@@ -1,14 +1,66 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 // import from child components...
+import InterestDisplay from '.././components/InterestDisplay.jsx'
+import PreSurvey from '.././components/PreSurvey.jsx'
 
 class ProfileContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      username: '',
+      interests: [],
+      interestsCaptured: true,
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.profilePage = this.profilePage.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    console.log(this.state)
+    if (target.type === 'checkbox' && this.state.interests.length === 2) console.log('too many clicks')
+    let value;
+    let name;
+    if (target.type === 'checkbox' && this.state.interests.length < 3) {
+      value = [...this.state.interests, target.name];
+      name = 'interests';
+    }
+    else {
+      value = target.value;
+      name = target.name;
+    }
+    this.setState({
+      [name]: value
+    });
+  }
+
+  profilePage() {
+
+    if (this.state.interestsCaptured) {
+      return (
+        <div className="container">
+          <div className="outerBox">
+            <h2>Welcome Bob!</h2>
+            <div>This is your profile page</div>
+            <InterestDisplay events={this.props.events} interests={this.props.interests}/>
+          </div>
+        </div>
+      )
+    }
+
+    else {
+      return (
+        <div><PreSurvey /></div>
+      )
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h2>I'm the profileContainer</h2>
-      </div>
-    )
+      <div>{this.profilePage()}</div>
+    );
   }
 }
 
