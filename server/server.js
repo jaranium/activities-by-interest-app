7a008dbef40e6d84c469d0119db6810f89f89455
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-03-31 09:16:07 -0700

    server now listening on port 5555

--- server/server.js
+++ server/server.js
@@ -1,26 +1,36 @@
 const express = require('express');
-const router = express.Router()
+// const router = express.Router()
+const router = express();
 //const app = express(); Do we need 'app' if we have express.router?
 const pg = require('pg');
 const path = require('path')
-const db = 'postgres://localhost:5555/interest_app';
+const bodyParser = require('body-Parser');
+const connectionString = 'postgres://localhost:5555/interest_app';
 // Do we have imports from another file?
 
-const client = new pg.Client(connectionString);
+const client = new pg.Client({
+  host: connectionString,
+  port: 5555
+});
+
 client.connect();
 
-router.post('/', (req, res, next) => {
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+router.post('/api/signup', (req, res, next) => {
+  console.log("req.body in user post req", req.body)
+
   const userInfoChunk = [];
   const loginInfo = {
-    username: req.body.username,
+    // username: req.body.username,
     password: req.body.password,
     email: req.body.email
   }
   pg.connect(connectionString, (err, client, done) => {
     if(err) {
-      done();
       console.log(err, 'SOMETHING IS NOT RIGHT');
-      return res.status(500).json({success: false, data: err})
+      return res.status(500).json({success: false, data: err})  //return automatically ends this middleware
     }
     client.query('INSERT INTO users(user_id, password, email) values(req.body.username, req.body.password, req.body.email)')
     query.on('row', (row) => {
@@ -36,3 +46,8 @@ router.post('/', (req, res, next) => {
 //   console.log(req.body)
 //   res.sendFile(path.join(__dirname, './../profile'))
 // });
+
+const port = 5555;
+router.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = router;
